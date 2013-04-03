d211cd07adfe21961545f0df762923f2d10e6249
Luis Román <luisromangz@gmail.com>
2013-04-03 17:16:29 +0100

    Fixes #6, we now have a TCPDF subclass that does all the PDF generation work.

---
+++ ParametrizedPDF.php
@@ -0,0 +1,364 @@
+<? require_once("lib/tcpdf/tcpdf.php");
+
+class ParametrizedPDF extends TCPDF {
+
+	private function applyNewFont($newFont) {
+		$newFamily = getOptionalParam("family", $newFont, $this->getFontFamily());
+
+		$newStyle = getOptionalParam("style", $newFont, $this->getFontStyle());
+
+		$newSize = getOptionalParam("size", $newFont, $this->getFontSizePt());
+
+		$this->SetFont($newFamily, $newStyle, $newSize);
+	}
+
+	private function addTextItem($textItem, $idx) {
+		$text = getRequiredParam("text", $textItem, $idx);
+
+		$align = getOptionalParam("align", $textItem, "L");
+
+		$newLine = getOptionalParam("newLine", $textItem, true);
+		$newLine = $newLine ? 1 : 0;
+
+		$height = $this->getLineHeight($this);
+		// We decode the string as in PDFs content UTF8 is not supported
+		$this->Cell(0, $height, $text, 0, $newLine, $align);
+	}
+
+	private function addParItem($parItem, $idx) {
+		$text = getRequiredParam("text", $parItem, $idx);
+		$align = getOptionalParam("align", $parItem, "L");
+		$width = getOptionalParam("width", $parItem, 0);
+
+		$lineHeight = $this->getLineHeight($this);
+		$this->MultiCell($width, $lineHeight, $text, 0, $align);
+	}
+
+	private function addImageItem($imageItem, $idx) {
+		$pngImage = null;
+
+		if (array_key_exists("url", $imageItem)) {
+
+			// Image url can be a file in the server's filesystem, an url, or a data uri.
+			$imageURL = $imageItem["url"];
+
+			if (strpos($imageURL, "data:") === 0) {
+				$pngImage = $this->imageFromDataUri($imageURL, $idx);
+			} else {
+				// We try retrieving a remote image.
+				$pngImage = $this->imageFromRemoteUrl($imageURL, $idx);
+			}
+
+		} else if (array_key_exists("fileInputName", $imageItem)) {
+			// The file came as an uploaded file in an multipart post request.
+			$fileInputName = $imageItem["fileInputName"];
+			$pngImage = $this->imageFromUpload($fileInputName, $idx);
+		} else {
+			showError("Either 'url' or 'fileInputName' must be specified for imageItem at position $idx");
+		}
+
+		// We retrieve the image's size.
+		$imageWidthPx = imagesx($pngImage);
+		$imageHeightPx = imagesy($pngImage);
+
+		// Conversion between mm and px. 1 inch = 25.4 mm, and standard PDF resolution is 72 dpi (px/inch).
+		$pxToMM = 25.4 / 72;
+
+		$iWidth = $imageWidthPx * $pxToMM;
+		$iHeight = $imageHeightPx * $pxToMM;
+
+		// Optional params "width" and "height"	
+		$width = $iWidth;
+		$height = $iHeight;
+		if (array_key_exists("width", $imageItem) && array_key_exists("height", $imageItem)) {
+			$width = $imageItem["width"];
+			$height = $imageItem["height"];
+		} else if (array_key_exists("width", $imageItem)) {
+			// If only one param is specified, we keep the aspect ratio.
+			$width = $imageItem["width"];
+			$height = $width * $iHeight / $iWidth;
+		} else if (array_key_exists("height", $imageItem)) {
+			$height = $imageItem["height"];
+			$width = $height * $iWidth / $iHeight;
+		}
+
+		// We save the image in a tmp file to be able to load it.
+		$imagePath = tempnam(sys_get_temp_dir(), "imageTmp");
+
+		imagealphablending($pngImage, false);
+		imagesavealpha($pngImage, true);
+		imagepng($pngImage, $imagePath, 1);
+
+		// Page break disabled before adding the image, or it might be downscaled to fit the page even if
+		// the Image method receives a fitonpage false by default.
+		$this->SetAutoPageBreak(false);
+		// We specify PNG as the format as we always convert the image or PDF to PNG.
+		$this->Image($imagePath, $this->GetX(), $this->GetY(), $width, $height, "PNG");
+		$margins = $this->GetMargins();
+		$this->SetAutoPageBreak(true, $margins["bottom"]);
+		$this->SetY($this->GetY() + $height);
+
+		unlink($imagePath);
+
+		imagedestroy($pngImage);
+	}
+
+	private function imageFromDataUri($imageURL, $idx) {
+		// We get the image's content.
+		$imgData = base64_decode(substr($imageURL, strpos($imageURL, ",") + 1));
+
+		$pngImage = $this->imageFromContents($imgData, $idx);
+		if (!$pngImage) {
+			showError("The data uri provided as url parameter for image item at position $idx doesn't contain a valid image or PDF.");
+		}
+
+		return $pngImage;
+	}
+
+	private function imageFromRemoteUrl($url, $idx) {
+
+		$imageContent = $this->remoteRequest($url);
+
+		// Handle response
+		$srcImage = $this->imageFromContents($imageContent);
+
+
+		if (!$srcImage) {
+			showError("The url specified for image item at position $idx didn't contain a valid image.");
+		}
+
+		// Close the connetion
+		curl_close($session);
+
+		return $srcImage;
+	}
+
+	private function remoteRequest($url, $idx) {
+		// Defining the default CURL options
+		$defaults = array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => TRUE);
+
+		// Open the Curl session
+		$session = curl_init();
+
+		// Setting the options
+		curl_setopt_array($session, $defaults);
+
+		// Make the call
+		$remoteContent = curl_exec($session);
+
+		// Close the connetion
+		curl_close($session);
+
+		if (!$remoteContent) {
+			showError("Curl couldn't retrieve the content of url ".$url.
+			" specified in the item at position $idx. Error was: ".curl_error($session));
+		}
+
+		return $remoteContent;
+	}
+
+	private function imageFromUpload($formFieldName, $idx) {
+		if (!array_key_exists($formFieldName, $_FILES)) {
+			showError("No uploaded file found for file input name '$fileInputName' specified for item at position $idx");
+		}
+
+		// We try opening the uploaded file we don't trust mime type or extensions.
+		$filePath = $_FILES[$formFieldName]["tmp_name"];
+
+
+		$resultImg = $this->imageFromContents(file_get_contents($filePath));
+
+		if (!$resultImg) {
+			showError("The image uploaded in field '$formFieldName' for image item at $idx has an invalid format.");
+		}
+
+		unlink($filePath);
+
+		return $resultImg;
+
+	}
+
+	private function imageFromContents($fileContents) {
+		$img = @imagecreatefromstring($fileContents);
+
+
+		if (!$img) {
+			// We try converting this from pdf.
+			$tmpPDFInput = tempnam(sys_get_temp_dir(), "pdfInput");
+			file_put_contents($tmpPDFInput, $fileContents);
+
+
+			$img = convertPDFFileToImage($tmpPDFInput);
+
+			unlink($tmpPDFInput);
+		}
+		return $img;
+	}
+
+
+	private function addTableItem($tableItem, $idx) {
+
+		$rows = getRequiredParam("rows", $tableItem, $idx);
+
+		$left = $this->GetX();
+		$top = $this->GetY();
+
+		$this->setDrawColor(0, 0, 0);
+
+		$borderWidth = $this->GetLineWidth();
+
+		$cellPadding = getOptionalParam("cellpadding", $tableItem, 2);
+
+		$htmlTable = "<table border=\"$borderWidth\" cellpadding=\"$cellPadding\">";
+
+		$vAlignHeightHack = $this->GetFontSize() * 2;
+
+		for ($rowIdx = 0; $rowIdx < count($rows); $rowIdx++) {
+			$htmlTable.= "<tr>";
+
+			$row = $rows[$rowIdx];
+			for ($colIdx = 0; $colIdx < count($row); $colIdx++) {
+				$column = $row[$colIdx];
+
+				$colSpan = 1;
+				$rowSpan = 1;
+				$hAlign = "left";
+				$vAlign = "top";
+				$columnText = $column;
+				$width = "auto";
+
+				if (is_array($column)) {
+					if (!array_key_exists("text", $column)) {
+						showError("'text' must be defined for the cell $cIdx of row $rIdx of tableItem at position $idx");
+					}
+					$columnText = $column["text"];
+
+					$colSpan = getOptionalParam("colspan", $column, 1);
+					$rowSpan = getOptionalParam("rowspan", $column, 1);
+					$hAlign = getOptionalParam("align", $column, "left");
+					$vAlign = getOptionalParam("valign", $column, "top");
+					$width = getOptionalParam("width", $column, "auto");
+				}
+
+				if ($rowSpan > 1) {
+					switch ($vAlign) {
+						case 'bottom':
+							$columnText = "<span style=\"font-size: $vAlignHeightHack;\">".str_repeat('&nbsp;<br/>', $rowSpan).
+							'</span>'.$columnText;
+							break;
+						case 'middle':
+							$columnText = "<span style=\"font-size: $vAlignHeightHack;\">".str_repeat('&nbsp;<br/>', $rowSpan - 1).
+							'</span>'.$columnText;
+							break;
+					}
+				}
+
+				if ($width === "auto") {
+					$htmlTable.= "<td align=\"$hAlign\" colspan=\"$colSpan\" rowspan=\"$rowSpan\">$columnText</td>";
+				} else {
+					$width.= "mm";
+					$htmlTable.= "<td align=\"$hAlign\" colspan=\"$colSpan\" rowspan=\"$rowSpan\" width=\"$width\">$columnText</td>";
+				}
+
+			}
+
+			$htmlTable.= "</tr>";
+		}
+
+		$htmlTable.= "</table>";
+
+		$this->writeHTML($htmlTable, false, false, false, false, '');
+	}
+
+	private function addHtmlItem($htmlItem, $idx) {
+		$htmlContent = getOptionalParam("content", $htmlItem, "");
+
+		if (!$htmlContent) {
+			$url = getOptionalParam("url", $htmlItem, false);
+			if (!$url) {
+				showError("Either 'url' or 'content' must be specified for the html item at position $idx");
+			}
+
+			$htmlContent = $this->remoteRequest($url, $idx);
+		}
+
+		$this->writeHTML($htmlContent, false, false, false, false, '');
+	}
+
+
+	private function getLineHeight() {
+		// A possibly very bad appoximation.
+		return  $this->GetStringWidth("x")*1.5;
+	}
+
+
+	public function addItem($item, $idx) {
+		// Here we do the common stuff.
+
+		// We set the font styles that will be used in this items and following ones
+		// (if not changed again).
+		if (array_key_exists("newFont", $item)) {
+			applyNewFont($item["newFont"]);
+		}
+
+		$lineWidth = getOptionalParam("newLineWidth", $item, $this->GetLineWidth());
+		$this->SetLineWidth($lineWidth);
+
+		$type = getOptionalParam("type", $item, "text");
+
+
+		$x = $this->GetX();
+		if (array_key_exists("x", $item)) {
+			// Absolute positioning.
+			$x = intval($item["x"]);
+
+		} else if (array_key_exists("dx", $item)) {
+			// Relative
+			$x += intval($item["dx"]);
+		}
+
+
+		$y = $this->GetY();
+		if (array_key_exists("y", $item)) {
+			// Absolute positioning.
+			$y = intval($item["y"]);
+		} else if (array_key_exists("dy", $item)) {
+			// Relative
+			$y += intval($item["dy"]);
+		}
+
+		$this->SetXY($x, $y);
+
+		switch (strtolower($type)) {
+			case "text":
+				$this->addTextItem($item, $idx);
+				break;
+			case "paragraph":
+			case "par":
+				$this->addParItem($item, $idx);
+				break;
+			case "image":
+				$this->addImageItem($item, $idx);
+				break;
+			case "table":
+				$this->addTableItem($item, $idx);
+				break;
+			case "html":
+				$this->addHtmlItem($item, $idx);
+				break;
+			default:
+				showError("Unsupported item type for item ".$idx);
+		}
+
+		// By default, we advance the position so we are placed after the element.
+		$keepPosition = getOptionalParam("keepPosition", $item, false);
+		if ($keepPosition) {
+			// We keep the initial position, so we reset the calculated x and y.
+			$this->SetXY($x, $y);
+		}
+
+	}
+
+}
--- phpPDF.php
+++ phpPDF.php
@@ -1,390 +1,8 @@
 <?
+
 ini_set('max_execution_time', 300);
-require_once("lib/tcpdf/tcpdf.php");
 
-function showError($errMesg) {
-	error_log("phpPDF: ".$errMesg);
-	$response = array(
-		"error" => "phpPDF: ".$errMesg);
-	
-	header("Content-Type: application/json");  
-	echo stripslashes(json_encode($response));	
-	exit(1);
-}
-
-function applyNewFont($pdf, $newFont) {
-	$newFamily = getOptionalParam("family",$newFont,$pdf->getFontFamily());
-	
-	$newStyle = getOptionalParam("style",$newFont,$pdf->getFontStyle());
-	
-	$newSize = getOptionalParam("size",$newFont,$pdf->getFontSizePt());
-
-	$pdf->SetFont($newFamily, $newStyle, $newSize);
-}
-
-function addTextItem($pdf, $textItem, $idx) {
-	$text = getRequiredParam("text",$textItem,$idx);
-
-	$align = getOptionalParam("align",$textItem,"L");
-	
-	$newLine = getOptionalParam("newLine",$textItem,true);
-	$newLine = $newLine?1:0;
-
-	$height = getLineHeight($pdf);
-	// We decode the string as in PDFs content UTF8 is not supported
-	$pdf->Cell(0, $height, $text,0,$newLine,$align);
-}
-
-function addParItem($pdf, $parItem, $idx) {
-	$text = getRequiredParam("text",$parItem,$idx);
-	$align = getOptionalParam("align",$parItem,"L");
-	$width= getOptionalParam("width", $parItem, 0);
-
-	$lineHeight = getLineHeight($pdf);
-	$pdf->MultiCell($width,$lineHeight, $text, 0, $align);	
-}
-
-function addImageItem($pdf, $imageItem, $idx) {
-	$pngImage = null;
-
-	if(array_key_exists("url", $imageItem)) {
-
-		// Image url can be a file in the server's filesystem, an url, or a data uri.
-		$imageURL  = $imageItem["url"];
-
-		if(strpos($imageURL,"data:")===0) {
-			$pngImage = imageFromDataUri($imageURL, $idx);
-		} else  {
-			// We try retrieving a remote image.
-			$pngImage = imageFromRemoteUrl($imageURL, $idx);
-		}
-
-	} else if(array_key_exists("fileInputName", $imageItem)) {
-		// The file came as an uploaded file in an multipart post request.
-		$fileInputName = $imageItem["fileInputName"];		
-		$pngImage = imageFromUpload($fileInputName, $idx);
-	} else {
-		showError("Either 'url' or 'fileInputName' must be specified for imageItem at position $idx");
-	}
-
-	// We retrieve the image's size.
-	$imageWidthPx = imagesx($pngImage);	
-	$imageHeightPx = imagesy($pngImage);
-
-	// Conversion between mm and px. 1 inch = 25.4 mm, and standard PDF resolution is 72 dpi (px/inch).
-	$pxToMM = 25.4/72;
-
-	$iWidth = $imageWidthPx*$pxToMM;
-	$iHeight = $imageHeightPx*$pxToMM;
-	
-	// Optional params "width" and "height"	
-	$width = $iWidth;
-	$height = $iHeight;
-	if(array_key_exists("width",$imageItem) && array_key_exists("height",$imageItem)) {
-		$width = $imageItem["width"];
-		$height = $imageItem["height"];
-	} else if(array_key_exists("width",$imageItem)){
-		// If only one param is specified, we keep the aspect ratio.
-		$width = $imageItem["width"];
-		$height = $width*$iHeight/$iWidth;
-	} else if(array_key_exists("height",$imageItem)){
-		$height = $imageItem["height"];
-		$width = $height*$iWidth/$iHeight;
-	} 
-	
-	// We save the image in a tmp file to be able to load it.
-	$imagePath = tempnam(sys_get_temp_dir(),"imageTmp");
-
-	imagealphablending($pngImage, false);
-	imagesavealpha($pngImage, true);
-	imagepng($pngImage, $imagePath, 1);
-
-	// Page break disabled before adding the image, or it might be downscaled to fit the page even if
-	// the Image method receives a fitonpage false by default.
-	$pdf->SetAutoPageBreak(false);
-	// We specify PNG as the format as we always convert the image or PDF to PNG.
-	$pdf->Image($imagePath, $pdf->GetX(), $pdf->GetY(), $width, $height, "PNG");
-	$margins = $pdf->GetMargins();
-	$pdf->SetAutoPageBreak(true, $margins["bottom"]);
-	$pdf->SetY($pdf->GetY()+$height);
-
-	unlink($imagePath);
-
-	imagedestroy($pngImage);
-}
-
-function imageFromDataUri($imageURL, $idx) {
-	// We get the image's content.
-	$imgData = base64_decode(substr($imageURL, strpos($imageURL, ",")+1));
-
-	$pngImage = imageFromContents($imgData, $idx);
-	if(!$pngImage) {
-		showError("The data uri provided as url parameter for image item at position $idx doesn't contain a valid image or PDF.");
-	}
-
-	return $pngImage;
-}
-
-function imageFromRemoteUrl( $url, $idx) {
-	
-	$imageContent = remoteRequest($url);
-
-	// Handle response
-	$srcImage = imageFromContents($imageContent);
-	
-
-	if(!$srcImage) {
-		showError("The url specified for image item at position $idx didn't contain a valid image.");
-	}
-
-	// Close the connetion
-	curl_close($session);
-
-	return $srcImage;
-}
-
-function remoteRequest($url, $idx) {
-	// Defining the default CURL options
-	$defaults = array( 
-	        CURLOPT_URL => $url, 
-	        CURLOPT_RETURNTRANSFER => TRUE	  
-	 ); 
-
-	// Open the Curl session
-	$session = curl_init();		    
-
-	// Setting the options
-	curl_setopt_array($session, $defaults);		    
-
-	// Make the call
-	$remoteContent = curl_exec($session);	
-
-	// Close the connetion
-	curl_close($session);
-
-	if(!$remoteContent) {
-		showError("Curl couldn't retrieve the content of url ".$url." specified in the item at position $idx. Error was: ".curl_error($session));
-	}
-
-	return $remoteContent;
-}
-
-function imageFromUpload($formFieldName, $idx) {
-	if(!array_key_exists($formFieldName, $_FILES)) {
-		showError("No uploaded file found for file input name '$fileInputName' specified for item at position $idx");
-	}
-
-	// We try opening the uploaded file we don't trust mime type or extensions.
-	$filePath = $_FILES[$formFieldName]["tmp_name"];
-
-
-	$resultImg = imageFromContents(file_get_contents($filePath));
-
-	if(!$resultImg){
-		showError("The image uploaded in field '$formFieldName' for image item at $idx has an invalid format.");
-	}
-	
-	unlink($filePath);
-
-	return $resultImg;
-
-}
-
-function imageFromContents($fileContents) {
-	$img = @imagecreatefromstring($fileContents);
-
-
-	if(!$img) {
-		// We try converting this from pdf.
-		$tmpPDFInput = tempnam(sys_get_temp_dir(), "pdfInput");
-		file_put_contents($tmpPDFInput, $fileContents);
-
-		
-		$img = convertPDFFileToImage($tmpPDFInput);
-
-		unlink($tmpPDFInput);
-	}
-	return $img;
-}
-
-function convertPDFFileToImage($filePath) {
-	$img = new imagick(); 
-	   
-    $img->setResolution(210,210);	
-    $img->readImage($filePath);
-    
-    $img->resetIterator();
-
-    // If the PDF has several pages, we merge all of them in one image.
-    $img = $img->appendImages(true);
-    $img->setImageFormat( "png" );    
-    $img->adaptiveBlurImage(1,1);
-    $img->setImageUnits(imagick::RESOLUTION_PIXELSPERINCH);	  
-
-    $data = $img->getImageBlob(); 
-
-    $img = imagecreatefromstring($data);
-
-    return $img;
-}
-
-
-function addTableItem($pdf, $tableItem, $idx) {
-
-	$rows = getRequiredParam("rows", $tableItem, $idx);
-
-	$left = $pdf->GetX();
-	$top = $pdf->GetY();
-
-	$pdf->setDrawColor(0,0,0);
-
-	$borderWidth = $pdf->GetLineWidth();
-
-	$cellPadding = getOptionalParam("cellpadding",$tableItem, 2);
-
-	$htmlTable = "<table border=\"$borderWidth\" cellpadding=\"$cellPadding\">";
-
-	$vAlignHeightHack = $pdf->GetFontSize()*2;
-
-	for($rowIdx = 0; $rowIdx < count($rows); $rowIdx++) {
-		$htmlTable.="<tr>";
-
-		$row = $rows[$rowIdx];
-		for($colIdx=0; $colIdx < count($row) ; $colIdx++){
-			$column = $row[$colIdx];
-
-			$colSpan = 1;
-			$rowSpan = 1;
-			$hAlign = "left";
-			$vAlign = "top";
-			$columnText = $column;
-			$width = "auto";
-
-			if(is_array($column)) {
-				if(!array_key_exists("text",$column)) {
-					showError("'text' must be defined for the cell $cIdx of row $rIdx of tableItem at position $idx");
-				}
-				$columnText = $column["text"];
-
-				$colSpan = getOptionalParam("colspan", $column, 1);
-				$rowSpan = getOptionalParam("rowspan", $column, 1);
-				$hAlign = getOptionalParam("align", $column, "left");
-				$vAlign = getOptionalParam("valign", $column, "top");
-				$width = getOptionalParam("width",$column,"auto");
-			}
-
-			if($rowSpan>1) {
-				switch ($vAlign) {
-					case 'bottom':
-						$columnText="<span style=\"font-size: $vAlignHeightHack;\">"
-							.str_repeat('&nbsp;<br/>', $rowSpan).'</span>'.$columnText;
-						break;
-					case 'middle':
-						$columnText="<span style=\"font-size: $vAlignHeightHack;\">"
-							.str_repeat('&nbsp;<br/>', $rowSpan-1).'</span>'.$columnText;
-						break;
-				}
-			}
-
-			if($width==="auto") {
-				$htmlTable.="<td align=\"$hAlign\" colspan=\"$colSpan\" rowspan=\"$rowSpan\">$columnText</td>";
-			} else {
-				$width.="mm";
-				$htmlTable.="<td align=\"$hAlign\" colspan=\"$colSpan\" rowspan=\"$rowSpan\" width=\"$width\">$columnText</td>";	
-			}
-			
-		}
-
-		$htmlTable.="</tr>";
-	}
-	
-	$htmlTable.="</table>";
-
-	$pdf->writeHTML($htmlTable, false, false, false, false, '');
-}
-
-function addHtmlItem($pdf, $htmlItem, $idx) {
-	$htmlContent = getOptionalParam("content", $htmlItem,"");
-
-	if(!$htmlContent) {
-		$url = getOptionalParam("url", $htmlItem, false);
-		if(!$url) {
-			showError("Either 'url' or 'content' must be specified for the html item at position $idx");
-		}
-
-		$htmlContent = remoteRequest($url,$idx);
-	}
-
-	$pdf->writeHTML($htmlContent, false, false, false, false, '');
-}
-
-function addItem ($pdf, $item, $idx) {
-	// Here we do the common stuff.
-
-	// We set the font styles that will be used in this items and following ones
-	// (if not changed again).
-	if(array_key_exists("newFont", $item)) {
-		applyNewFont($pdf, $item["newFont"]);
-	}
-
-	$lineWidth = getOptionalParam("newLineWidth",$item, $pdf->GetLineWidth());
-	$pdf->SetLineWidth($lineWidth);
-
-	$type = getOptionalParam("type",$item,"text");
-
-
-	$x = $pdf->GetX();
-	if(array_key_exists("x", $item)) {
-		// Absolute positioning.
-		$x = intval($item["x"]);
-
-	} else if(array_key_exists("dx", $item)) {
-		// Relative
-		$x+=intval($item["dx"]);
-	}
-	
-
-	$y = $pdf->GetY();
-	if(array_key_exists("y", $item)) {
-		// Absolute positioning.
-		$y = intval($item["y"]);
-	} else if(array_key_exists("dy", $item)) {
-		// Relative
-		$y+=intval($item["dy"]);
-	}
-
-	$pdf->SetXY($x,$y);
-
-	switch(strtolower($type)) {
-		case "text":
-			addTextItem($pdf, $item, $idx);
-			break;
-		case "paragraph":
-		case "par":
-		 	addParItem($pdf, $item, $idx);
-			break;
-		case "image":
-			addImageItem($pdf, $item, $idx);
-			break;
-		case "table":
-			addTableItem($pdf, $item, $idx);
-			break;
-		case "html":
-			addHtmlItem($pdf, $item, $idx);
-			break;
-		default:
-			showError("Unsupported item type for item ". $idx);
-	}
-
-	// By default, we advance the position so we are placed after the element.
-	$keepPosition = getOptionalParam("keepPosition", $item, false);
-	if($keepPosition) {
-		// We keep the initial position, so we reset the calculated x and y.
-		$pdf->SetXY($x,$y);
-	}
-
-}
-
+require_once("ParametrizedPDF.php");
 
 function getRequiredParam($paramName, $item, $itemIdx) {
 	if(!array_key_exists($paramName, $item)) {
@@ -403,9 +21,35 @@ function getOptionalParam ($paramName, $item, $defaultValue) {
 	return $result;
 }
 
-function getLineHeight($pdf) {
-	// A possibly very bad appoximation.
-	return  $pdf->GetStringWidth("x")*1.5;
+function convertPDFFileToImage($filePath) {
+	$img = new imagick();
+
+	$img->setResolution(210, 210);
+	$img->readImage($filePath);
+
+	$img->resetIterator();
+
+	// If the PDF has several pages, we merge all of them in one image.
+	$img = $img->appendImages(true);
+	$img->setImageFormat("png");
+	$img->adaptiveBlurImage(1, 1);
+	$img->setImageUnits(imagick::RESOLUTION_PIXELSPERINCH);
+
+	$data = $img->getImageBlob();
+
+	$img = imagecreatefromstring($data);
+
+	return $img;
+}
+
+function showError($errMesg) {
+	error_log("phpPDF: ".$errMesg);
+	$response = array(
+		"error" => "phpPDF: ".$errMesg);
+
+	header("Content-Type: application/json");
+	echo stripslashes(json_encode($response));
+	exit(1);
 }
 
 $params = null;
@@ -457,7 +101,6 @@ if($downloadFile) {
 }
 
 
-
 $paperSize = getOptionalParam("size",$params,"A4");
 $margin = getOptionalParam("margin", $params, 30);
 $pageOrientation = getOptionalParam("orientation", $params, "P");
@@ -471,7 +114,7 @@ if(array_key_exists("items",$params)) {
 
 $outputFile = getOptionalParam("outputFile",$params, $outputFormat==="PDF"?"doc.pdf":"doc.png");
 
-$pdf = new TCPDF($pageOrientation,"mm",$paperSize);
+$pdf = new ParametrizedPDF($pageOrientation,"mm",$paperSize);
 
 // We set the file's metadata. It won't be preserved if output is 'PNG'.
 $pdf->SetTitle(getOptionalParam("title",$params,""));
@@ -489,7 +132,7 @@ $pdf->SetFontSize(12);
 
 // We add items to the pdf!
 for($i=0; $i < count($items); $i++) {
-	addItem($pdf, $items[$i], $i);
+	$pdf->addItem($items[$i], $i);
 }
 
 
